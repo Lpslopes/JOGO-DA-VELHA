30f0821bcc87b19158566a6dd7e25c8811b4bc58
Lpslopes <lamartinelps@gmail.com>
2021-07-03 22:54:15 -0300

    Funcionamento_do_jogo

--- interface.js
+++ interface.js
@@ -10,5 +10,32 @@ document.addEventListener('DOMContentLoaded', ()=>{
 })
 
 function handleClick(event) {
-    console.log(event.target);
+    
+    let square = event.target;
+    let position = square.id;
+
+    if (handleMove(position)){
+
+        setTimeout(() => {
+            alert("O Jogo acabou!! - O Vencedor foi" + playerTime);
+        }, 10);
+        
+    };
+    updateSquares();
+}
+
+function updateSquares() {
+
+    let squares = document.querySelectorAll(".square");
+
+    squares.forEach((square) => {
+        let position = square.id;
+        let symbol = board[position];
+
+        if (symbol != '') {
+            square.innerHTML = `<div class="${symbol}"></div>`;
+        }
+
+    })
+
 }
